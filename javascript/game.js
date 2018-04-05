b8d729dc38c391da1065ce113c0ac85a52770964
Zyris Shakir <zeyeland@gmail.com>
2018-04-05 16:20:11 -0500

    Version 1.1.1

--- javascript/game.js
+++ javascript/game.js
@@ -4,6 +4,7 @@ var ctx = canvas.getContext('2d');
 var player1 = new playerComponnet()
 
 function update(){
+    ctx.clearRect(0, 0, 100, 100);
     player1.update();
     ctx.drawImage(player1.playerImage,player1.x,player1.y,200,200);
  }
