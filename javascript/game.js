2a39dd03fe058c462068c4976da3acb9d1f70713
Zyris Shakir <zeyeland@gmail.com>
2018-04-05 15:47:59 -0500

    Version 1.2.0 - this version includes player controls

--- javascript/game.js
+++ javascript/game.js
@@ -3,13 +3,13 @@ var ctx = canvas.getContext('2d');
 
 var player1 = new playerComponnet()
 
-
-
 function update(){
     player1.update();
     ctx.drawImage(player1.playerImage,player1.x,player1.y,200,200);
  }
 setInterval(update,40);
+
+
 
 
 
--- javascript/player.js
+++ javascript/player.js
@@ -1,16 +1,35 @@
+//playerComponnet
 function playerComponnet(){
     this.health;
     this.x = 100;
-    this.spdX = 10;
+    this.spdX = 10; // player speed
     this. y = 100;
-    this.spdY= 10;
-    this.playerImage = new Image();
+    this.spdY= 10;  // player speed
+    this.playerImage = new Image(); //player img with src
     this.playerImage.src = "assets/apple.jpg";
 
-    this.update = function(newX, newY){
-      this.x += this.spdX;
-      this.y += this.spdY;
-    }
+    this.update = function(){
+        var parentThis = this;
+        document.onkeydown = function(e) {
+            switch (e.keyCode) {
+                case 37:
+                    parentThis.x -= parentThis.spdX;
+                    break;
+                case 38:
+                    parentThis.y -= parentThis.spdY;
+                    break;
+                case 39:
+                    parentThis.x += parentThis.spdX;
+                    break;
+                case 40:
+                    parentThis.y += parentThis.spdY;
+                    break;
+            }//end of switch
+        };   //end of document on keydown function 
 
+    }//end of update 
+   
+
+    
 
 }
