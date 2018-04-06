ce754ad21dce9486370f03be1de4af0f3970f08d
Zyris Shakir <zeyeland@gmail.com>
2018-04-06 14:55:34 -0500

    Version 1.2.2 - this version adds other piece to outside canvas

--- javascript/game.js
+++ javascript/game.js
@@ -3,10 +3,13 @@ var ctx = canvas.getContext('2d');
 
 var player1 = new playerComponnet()
 
+//ctx.scale(.5,.5);
+
 function update(){
-    ctx.scale(1,1);
+    //ctx.scale(1.1,1.1);
     ctx.clearRect(0, 0, 1000, 500);
     drawTiles();
+    drawTiles2();
     player1.update();
     ctx.drawImage(player1.playerImage,player1.x,player1.y,100,100);
  }
--- javascript/maps.js
+++ javascript/maps.js
@@ -26,10 +26,10 @@ function drawTiles(){
  for(rowCount = 0; rowCount < mapColumn; rowCount++){
     for(columnCount = 0; columnCount < mapColumn; columnCount++){
         if(mapGrid[gridCount] == 0){
-            ctx.drawImage(rock_ground_tile,columnCount*50,rowCount*50,50,50);
+            ctx.drawImage(rock_ground_tile,columnCount*50,rowCount*50,100,100);
         }
         if(mapGrid[gridCount] == 9){
-            ctx.drawImage(rock_wall_tile,columnCount*50,rowCount*50,50,50);
+            ctx.drawImage(rock_wall_tile,columnCount*50,rowCount*50,100,100);
         }
         console.log("column is " + columnCount);
         console.log("gridcount is " + gridCount)
@@ -37,3 +37,11 @@ function drawTiles(){
     }//end of inner for
 }
 }//end of drawtile function
+
+
+function drawTiles2(){
+     ctx.drawImage(rock_wall_tile, 1500,700,100,100);
+     ctx.drawImage(rock_wall_tile, 1500,720,100,100);
+     ctx.drawImage(rock_wall_tile, 1500,740,100,100);
+     ctx.drawImage(rock_wall_tile, 1500,760,100,100);
+   }//end of drawtile function
