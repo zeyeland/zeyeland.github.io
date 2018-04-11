2ecda04a4d886643d7dceba39ba427ad8fd4223b
Zyris Shakir <zeyeland@gmail.com>
2018-04-11 09:16:59 -0500

    Version 1.4.0 - Sprite Animations

--- javascript/player.js
+++ javascript/player.js
@@ -2,75 +2,102 @@
 function playerComponnet(){
     this.health;
     this.x = 500; //500;
-    this.spdX = 25; // player speed
+    this.spdX = 50; // player speed
     this.y = 250; //250;
     this.spdY= 50;  // player speed
     this.playerImage = new Image(); //player img with src
-    this.playerImage.src = "assets/player_Sprites/person-Chill-Right.png";
+    this.playerImage.src = "assets/player_Sprites/playerChill-Left.png";
 
-    this.playerImage2 = new Image(); //player img with src
-    this.playerImage2.src = "assets/player_Sprites/person-Sword-Left.png";
+    /*player looking directions for sprites*/
+    this.lookLeft = true;
+    this.lookRight = false;
 
     this.update = function(){
         var parentThis = this;
         document.onkeydown = function(e) {
             switch (e.keyCode) {
-                case 37:
-                if(checkWallCollision("left") == false){
-                    parentThis.x -= parentThis.spdX;
-                    console.log("player left");
-                    offsetX--;
-                    console.log("offsetX = " + offsetX);
-                    console.log(player1.getPosition());
-                    slideScreen();
+                case 65:
+                if(parentThis.lookLeft == true){
+                    if(checkWallCollision("left") == false){
+                        parentThis.playerImage.src = "assets/player_Sprites/playerRun-Left.png";
+                        parentThis.x -= parentThis.spdX;
+                        console.log("player left");
+                        offsetX--;
+                        console.log("offsetX = " + offsetX);
+                        slideScreen();
+                    }
+                }
+                else{
+                    parentThis.playerImage.src = "assets/player_Sprites/playerChill-Left.png";
+                    parentThis.lookLeft = true;
+                    parentThis.lookRight = false;
                 }
                     break;
-                case 38:
+                case 87:
                 if(checkWallCollision("above") == false){
                     parentThis.y -= parentThis.spdY;
                     console.log("player up");
                     offsetY--;
                     console.log("offsetY = " + offsetY);
-                    console.log(player1.getPosition());
                     slideScreen();
                 }
                     break;
-                case 39:
-                if(checkWallCollision("right") == false){
-                    parentThis.x += parentThis.spdX;
-                    console.log("player right");
-                    offsetX++;
-                    console.log("offsetX = " + offsetX);
-                    console.log(player1.getPosition());
-                    slideScreen();
+                case 68:
+                if(parentThis.lookRight == true){
+                    if(checkWallCollision("right") == false){
+                        parentThis.playerImage.src = "assets/player_Sprites/playerRun-Right.png";
+                        parentThis.x += parentThis.spdX;
+                        console.log("player right");
+                        offsetX++;
+                        console.log("offsetX = " + offsetX);
+                        slideScreen();
+                    }
+                }
+                else{
+                    parentThis.playerImage.src = "assets/player_Sprites/playerChill-Right.png";
+                    parentThis.lookRight = true;
+                    parentThis.lookLeft = false;
                 }
                     break;
-                case 40:
+                case 83:
                 if(checkWallCollision("below") == false){
                     parentThis.y += parentThis.spdY;
                     console.log("player down");
                     offsetY++;
                     console.log("offsetY = " + offsetY);
-                    console.log(player1.getPosition());
                     slideScreen();
                 }
                     break;
             }//end of switch
         };   //end of document on keydown function 
+        document.onkeyup = function(e){
+             switch (e.keyCode) {
+                case 65:
+                    parentThis.playerImage.src = "assets/player_Sprites/playerChill-Left.png";
+                    
+                    break;
+                case 87:
+                
+                
+                    break;
+                case 68:
+                    parentThis.playerImage.src = "assets/player_Sprites/playerChill-Right.png";
+                
+                    break;
+                case 83:
+                
+                
+                    break;
+            }//end of key up switch
+        }//end of keyup function
         drawPlayer();
     }//end of update function
 
     this.getPosition = function(){
         console.log(this.x + " .. " + this.y);
-        //checkWallCollision();
-        //return (this.x, this.y);
     }
 } // end of playerComponnet
 
 function drawPlayer(){
-    //ctx.drawImage(player1.playerImage,0,0,100,100,player1.x,player1.y,400,400);
     ctx.drawImage(player1.playerImage,player1.x,player1.y,50,50);
-    //ctx.drawImage(player1.playerImage2,player1.x,player1.y,50,50);
-   
-    
 }
